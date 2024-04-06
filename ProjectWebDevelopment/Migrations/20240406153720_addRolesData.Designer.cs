ec17b8527f254d00bfb56c75794aa84d56e0fd20
janderup <62472078+janderup@users.noreply.github.com>
2024-04-06 17:38:01 +0200

    Added default data for the roles

--- ProjectWebDevelopment/Migrations/20240406153720_addRolesData.Designer.cs
+++ ProjectWebDevelopment/Migrations/20240406153720_addRolesData.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectWebDevelopment.Data;
 
@@ -10,9 +11,11 @@ using ProjectWebDevelopment.Data;
 namespace ProjectWebDevelopment.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240406153720_addRolesData")]
+    partial class addRolesData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.3");
